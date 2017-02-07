c97f4e8b544fba02b966cd1142944fd764c135cf
sixspades <divyanshu.1410669@kiet.edu>
2017-02-07 18:32:59 +0530

    one commit

---
+++ swapnil.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class newts
+{
+
+public static void main(String args[])
+{
+Scanner p = new Scanner (System.in);
+int n=p.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+a[i]=p.nextInt();
+int c[]=new int[n+1];
+for(int i=0;i<n;i++)
+c[a[i]]++;
+for(int i=1;i<=N;i++)
+if(!c[i])
+System.out.print(i+" ");
+
+}
+}
