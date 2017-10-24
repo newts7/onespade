0d811c8425ddbf2b01f68750cd0cbae37a3b0cad
Divyanshu Srivastava <srivastavadi12@gmail.com>
2017-10-24 15:46:36 +0530

    lol
    lol again

--- swapnil.java
+++ swapnil.java
@@ -9,6 +9,8 @@ public static void main(String args[])
   System.out.println("hello");
   
   System.out.println("hello2");
+   
+  System.out.println("hello3");
 Scanner p = new Scanner (System.in);
 int n=p.nextInt();
 int a[]=new int[n];
